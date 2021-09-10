51e0d06ec1b6e501e653935d64e26e767e980e46
Tali Avni <tali@aristo7.com>
2021-09-10 12:25:04 -0700

    integrate loading spinner

--- src/components/elem/DynamicDataDisplay.js
+++ src/components/elem/DynamicDataDisplay.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
+import React from "react";
+import PropTypes from "prop-types";
+import LoadingSpinner from "./LoadingSpinner";
 
 const DynamicDataDisplay = ({ children, hook }) => {
   const { data } = hook();
-  if(data === 0) return (
-
-    <div className="DynamicDataDisplay">
-      <h3>{children}</h3>
-      <div className="dataSection">
-        <p>Loading</p>
+  if (data === 0)
+    return (
+      <div className="DynamicDataDisplay">
+        <h3>{children}</h3>
+        <div className="dataSection">
+          <LoadingSpinner />
+        </div>
       </div>
-    </div>
-  );
+    );
   return (
-
     <div className="DynamicDataDisplay">
       <h3>{children}</h3>
       <div className="dataSection">
-        <p>{data.toFixed(1)}
+        <p>
+          {data.toFixed(1)}
           <span> Days</span>
         </p>
       </div>
@@ -29,7 +29,7 @@ const DynamicDataDisplay = ({ children, hook }) => {
 DynamicDataDisplay.propTypes = {
   children: PropTypes.node.isRequired,
   data: PropTypes.number.isRequired,
-  hook: PropTypes.func.isRequired
+  hook: PropTypes.func.isRequired,
 };
 
 export default DynamicDataDisplay;
--- src/components/features/LandingPage.js
+++ src/components/features/LandingPage.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import Charts from '../elem/Charts/ChartContainer';
+import React, { useContext } from "react";
+import Charts from "../elem/Charts/ChartContainer";
+import { PopulationContext } from "../wrappers/PopulationContext";
+import LoadingSpinner from "../elem/LoadingSpinner";
 
 const HomePage = () => {
+  const { loading } = useContext(PopulationContext);
 
   return (
     <section className="bodyContainer">
-      <Charts />
+      {!loading ? <Charts /> : <LoadingSpinner />}
     </section>
   );
 };
