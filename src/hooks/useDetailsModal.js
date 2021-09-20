f50fe0d6c2609bb0dc8eed2ae8b5bc4c56dcd0c2
Tali Avni <tali@aristo7.com>
2021-09-19 20:37:09 -0700

    delete old files

--- src/hooks/useDetailsModal.js
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export const useDetailsModal = () => {
-  const [showDetailsModal, setShowDetailsModal] = useState(false);
-  const toggleDetailsModal = () => setShowDetailsModal(!showDetailsModal);
-  return { showDetailsModal, toggleDetailsModal };
-};
--- src/services/getDetentions.js
+++
@@ -1,9 +0,0 @@
-export const getDetentions = (params) => {
-  return fetch(`https://jail-data-pdx.herokuapp.com/api/v1/detentions?${params.toString()}`)
-    .then(res => res.json());
-};
-
-export const getDetention = (id) => {
-  return fetch(`https://jail-data-pdx.herokuapp.com/api/v1/detentions/${id}`)
-    .then(res => res.json());
-};
