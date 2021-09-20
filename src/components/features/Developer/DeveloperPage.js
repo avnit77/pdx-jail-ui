952ce4cd40840ba63cf99bb1632ffe94ec5b2cf6
Tali Avni <tali@aristo7.com>
2021-09-19 20:31:52 -0700

    changes to dev page

--- src/components/features/Developer/DeveloperPage.js
+++ src/components/features/Developer/DeveloperPage.js
@@ -1,7 +1,44 @@
 /* eslint-disable react/jsx-no-target-blank */
 import React from "react";
+import withConfig from "../../wrappers/withConfig";
 
-const DeveloperPage = () => {
+const DeveloperPage = ({ config }) => {
+  const { API_URL } = config;
+  const params = {
+    persons: [
+      "?race=White",
+      "?gender=Male",
+      "count By Race",
+      "count By Gender",
+      "count By Age Range",
+    ],
+    detentions: [
+      "?arrestingAgency=Gresham+Police+Department",
+      "count By Agency",
+      "count By Time",
+      "avg Detention Duration",
+      "avg Detention By Race",
+      "avg Detention By Gender",
+      "count Charges By Race",
+      "count Charges By Gender",
+    ],
+    courtCases: [],
+  };
+
+  const createListItem = (param, header) => {
+    const fullURL = API_URL + param;
+    return (
+      <li>
+        <span className="routes">
+          {header ? header : `Get ALL ${param.replace(/([A-Z])/g, " $1")} - `}
+        </span>
+        <a className="paths" href={fullURL} target="_blank">
+          {!header ? fullURL : param}
+        </a>
+      </li>
+    );
+  };
+
   return (
     <>
       <main className="DeveloperPage section">
@@ -15,183 +52,40 @@ const DeveloperPage = () => {
         </p>
 
         <section className="smallSection">
-
-        <p className="notesEmphasis">
+          <p className="notesEmphasis">
             Get ALL routes are paged. Use ?page=[page number] and
             ?perPage=[records per page]
-        </p>
-        <div className="routes">
-          <h3 className="is-size-5">Person Routes</h3>
-          <ul>
-            <li>
-              <span className="routes">Get ALL persons -</span>
-              <a
-                className="paths"
-                href="https://jail-data-pdx.herokuapp.com/api/v1/persons"
-                target="_blank"
-              >
-                https://jail-data-pdx.herokuapp.com/api/v1/persons
-              </a>
-            </li>
-            <li>
-              <span className="routes">Query person subsets by keys - </span>
-              <a
-                className="paths"
-                href="https://jail-data-pdx.herokuapp.com/api/v1/persons/?race=White"
-                target="_blank"
-              >
-                /?race=White
-              </a>{" "}
-              or{" "}
-              <a
-                className="paths"
-                href="https://jail-data-pdx.herokuapp.com/api/v1/persons/?gender=Male"
-                target="_blank"
-              >
-                /?gender=Male
-              </a>
-            </li>
-            <li>
-              <span className="routes">Get count by RACE - </span>
-              <a
-                className="paths"
-                href="https://jail-data-pdx.herokuapp.com/api/v1/persons/countByRace"
-                target="_blank"
-              >
-                /countByRace
-              </a>
-            </li>
-            <li>
-              <span className="routes">Get count by GENDER - </span>
-              <a
-                className="paths"
-                href="https://jail-data-pdx.herokuapp.com/api/v1/persons/countByGender"
-                target="_blank"
-              >
-                /countByGender
-              </a>
-            </li>
-            <li>
-              <span className="routes">Get count by AGE - </span>
-              <a
-                className="paths"
-                href="https://jail-data-pdx.herokuapp.com/api/v1/persons/countByAgeRange"
-                target="_blank"
-              >
-                /countByAgeRange
-              </a>
-            </li>
-          </ul>
-          <br/>
-          <h3 className="is-size-5">Detention Routes</h3>
-          <ul>
-            <li>
-              <span className="routes">Get ALL detentions - </span>
-              <a
-                className="paths"
-                href="https://jail-data-pdx.herokuapp.com/api/v1/detentions"
-                target="_blank"
-              >
-                https://jail-data-pdx.herokuapp.com/api/v1/detentions
-              </a>
-            </li>
-            <li>
-              <span className="routes">Query detention subsets by keys - </span>
-              <a
-                className="paths"
-                href="https://jail-data-pdx.herokuapp.com/api/v1/detentions/?arrestingAgency=Gresham+Police+Department"
-                target="_blank"
-              >
-                /?arrestingAgency=Gresham+Police+Department
-              </a>
-            </li>
-            <li>
-              <span className="routes">Get count by AGENCY - </span>
-              <a
-                className="paths"
-                href="https://jail-data-pdx.herokuapp.com/api/v1/detentions/countByAgency"
-                target="_blank"
-              >
-                /countByAgency
-              </a>
-            </li>
-            <li>
-              <span className="routes">Get count by BOOKING TIME - </span>
-              <a
-                className="paths"
-                href="https://jail-data-pdx.herokuapp.com/api/v1/detentions/countByTime"
-                target="_blank"
-              >
-                /countByTime
-              </a>
-            </li>
-            <li>
-              <span className="routes">Get average detention DURATION - </span>
-              <a
-                className="paths"
-                href="https://jail-data-pdx.herokuapp.com/api/v1/detentions/avgDetentionDuration"
-                target="_blank"
-              >
-                /avgDetentionDuration
-              </a>
-            </li>
-            <li>
-              <span className="routes">Get average duration by RACE - </span>
-              <a
-                className="paths"
-                href="https://jail-data-pdx.herokuapp.com/api/v1/detentions/avgDetentionByRace"
-                target="_blank"
-              >
-                /avgDetentionByRace
-              </a>
-            </li>
-            <li>
-              <span className="routes">Get average detention by GENDER - </span>
-              <a
-                className="paths"
-                href="https://jail-data-pdx.herokuapp.com/api/v1/detentions/avgDetentionByGender"
-                target="_blank"
-              >
-                /avgDetentionByGender
-              </a>
-            </li>
-            <li>
-              <span className="routes">Get CHARGE count by RACE - </span>
-              <a
-                className="paths"
-                href="https://jail-data-pdx.herokuapp.com/api/v1/detentions/countChargesByRace"
-                target="_blank"
-              >
-                /countChargesByRace
-              </a>
-            </li>
-            <li>
-              <span className="routes">Get charge count by GENDER- </span>
-              <a
-                className="paths"
-                href="https://jail-data-pdx.herokuapp.com/api/v1/detentions/countChargesByGender"
-                target="_blank"
-              >
-                /countChargesByGender
-              </a>
-            </li>
-            <li>
-              <span className="routes">Get ALL court cases - </span>
-              <a
-                className="paths"
-                href="https://jail-data-pdx.herokuapp.com/api/v1/courtCases"
-                target="_blank"
-              >
-                https://jail-data-pdx.herokuapp.com/api/v1/courtCases
-              </a>
-            </li>
-          </ul>
-        </div>
+          </p>
+          <div className="routes">
+            {Object.keys(params).map((key) => {
+              return params[key].length ? (
+                <>
+                  <h3 className="is-size-5 routeHeader">{key} Routes</h3>{" "}
+                  <ul>
+                    {createListItem(key)}
+                    {params[key].map((value) => {
+                      const param = `${key}/${value.trim()}`;
+                      const title = value.includes("?")
+                        ? `Query ALL ${key} by keys - `
+                        : value.includes("avg")
+                        ? `Get Average ${value.slice(3)} - `
+                        : value.includes("Charges")
+                        ? `Get Charge Count ${value.slice(14)} - `
+                        : `Get ${value} -`;
+                      return createListItem(param, title);
+                    })}
+                  </ul>
+                </>
+              ) : (
+                createListItem(key)
+              );
+            })}
+          </div>
         </section>
-        <p className="notesEmphasis" style={{"textAlign": "center"}}>
-            PLEASE NOTE: While this project was originally developed in January of
-            2020, collection of data for the current version began on August 26,
-            2021.
+        <p className="notesEmphasis" style={{ textAlign: "center" }}>
+          PLEASE NOTE: While this project was originally developed in January of
+          2020, collection of data for the current version began on August 26,
+          2021.
         </p>
         <p className="is-size-7">
           Originally developed by{" "}
@@ -234,7 +128,7 @@ const DeveloperPage = () => {
           >
             Nick Brimmer
           </a>
-          . {<br/>} Currently being updated and maintained by{" "}
+          . {<br />} Currently being updated and maintained by{" "}
           <a
             href="https://github.com/avnit77"
             target="_blank"
@@ -249,4 +143,4 @@ const DeveloperPage = () => {
   );
 };
 
-export default DeveloperPage;
+export default withConfig(DeveloperPage);
